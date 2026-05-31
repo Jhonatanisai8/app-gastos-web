a6cb586cc6e8c6f1b44911d594eae5d3d2444088
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-30 22:12:24 -0500

    feat: configuracion de taildwind css

--- guardaBills/App.js
+++ guardaBills/App.js
@@ -1,12 +1,12 @@
 import { Text, View } from "react-native";
 import React, { Component } from "react";
-import "./global.css"
+import "./global.css";
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text> App </Text>{" "}
+      <View className="flex-1 items-center justify-center">
+        <Text className="bg-black text-xl font-bold p-1 text-white"> App </Text>
       </View>
     );
   }
