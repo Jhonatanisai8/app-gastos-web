d91cae9bc356c8382dbe184c5e448c8526c89546
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-30 23:10:13 -0500

    feat: inicio de sesion basic

--- guardaBills/src/api/apiClient.js
+++ guardaBills/src/api/apiClient.js
@@ -1,7 +1,7 @@
-const { default: axios } = require("axios");
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://10.0.2.2:8080/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
--- guardaBills/src/screens/RegistrationLogin.jsx
+++ guardaBills/src/screens/RegistrationLogin.jsx
@@ -5,14 +5,37 @@ import {
   TouchableOpacity,
   ScrollView,
   SafeAreaView,
+  Alert,
 } from "react-native";
 import React, { useState } from "react";
 import { Feather, Ionicons } from "@expo/vector-icons";
+import { useAuth } from "../hooks/useAuth";
 
 export function RegistrationLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [secureTextEntry, setSecureTextEntry] = useState(true);
+
+  //extraemos las funciones
+  const { login, loading, error, authData } = useAuth();
+
+  const handleLogin = async () => {
+    if (!email || !password) {
+      Alert.alert("Error", "Por favor llena todos los campos.");
+      return;
+    }
+
+    try {
+      const data = await login(email, password);
+      console.log(data);
+      Alert.alert("Login exitoso");
+    } catch (error) {
+      Alert.alert(
+        "Error de Autenticación",
+        error.message || "Credenciales incorrectas",
+      );
+    }
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-surface w-full">
@@ -102,6 +125,7 @@ export function RegistrationLogin() {
               <TouchableOpacity
                 className="w-full h-12 bg-primary items-center justify-center rounded-lg shadow-sm active:opacity-90"
                 activeOpacity={0.8}
+                onPress={handleLogin}
               >
                 <Text className="text-on-primary font-semibold text-sm">
                   Iniciar Sesión
